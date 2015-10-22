46160633d097939123840067f57a16bfd186e14e
Francisco Rojas <frojas@groupon.com>
2015-10-22 14:43:22 -0700

    Initial commit

---
+++ InstagramFeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstagramFeed
+//
+//  Created by Francisco Rojas Gallegos on 10/22/15.
+//  Copyright © 2015 Francisco Rojas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstagramFeed/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  InstagramFeed
+//
+//  Created by Francisco Rojas Gallegos on 10/22/15.
+//  Copyright © 2015 Francisco Rojas. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
