099bc63b062a2a069f3a7509acb2a0d0d98ea683
Francisco Rojas <frojas@groupon.com>
2015-10-22 14:58:27 -0700

    Add code to fetch images from instagram

--- InstagramFeed/PhotosViewController.m
+++ InstagramFeed/PhotosViewController.m
@@ -17,11 +17,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    [self fetchPhotos];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) fetchPhotos {
+    NSString *clientId = @"9f651fceb25a4b1ea6a460e97fe7a6f8";
+    NSString *urlString =
+    [@"https://api.instagram.com/v1/media/popular?client_id=" stringByAppendingString:clientId];
+
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+
+    NSURLSession *session =
+    [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]
+                                  delegate:nil
+                             delegateQueue:[NSOperationQueue mainQueue]];
+
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request
+                                            completionHandler:^(NSData * _Nullable data,
+                                                                NSURLResponse * _Nullable response,
+                                                                NSError * _Nullable error) {
+                                                if (!error) {
+                                                    NSError *jsonError = nil;
+                                                    NSDictionary *responseDictionary =
+                                                    [NSJSONSerialization JSONObjectWithData:data
+                                                                                    options:kNilOptions
+                                                                                      error:&jsonError];
+                                                    NSLog(@"Response: %@", responseDictionary);
+                                                } else {
+                                                    NSLog(@"An error occurred: %@", error.description);
+                                                }
+                                            }];
+    [task resume];
+
 }
 
 @end
